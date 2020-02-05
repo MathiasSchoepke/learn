112184ca02238e3c3cb913bd7bb31169860e3ebd
Mathias Schoepke <mathias.schoepke@outlook.de>
2020-02-05 19:10:29 +0100

    add manage tab design prototype

--- learn/src/main/java/mathiasschoepke/ManageBean.java
+++ learn/src/main/java/mathiasschoepke/ManageBean.java
@@ -1,8 +1,10 @@
 package mathiasschoepke;
 
 import java.io.Serializable;
+import java.util.List;
 
-import javax.faces.bean.ApplicationScoped;
+import javax.annotation.PostConstruct;
+//import javax.faces.bean.ApplicationScoped;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
@@ -16,8 +18,20 @@ import lombok.Setter;
 public class ManageBean implements Serializable {
 	private static final long serialVersionUID = -4829244622184322249L;
 
+	@Getter
+	private List<QuestionOpject> questionList;
+
+	@Getter
+	@Setter
+	private QuestionOpject selectedQuestion;
+
 	@ManagedProperty(value="#{dataBean}")
 	@Getter(AccessLevel.PUBLIC)
 	@Setter
 	private DataBean dataBean;
+
+	@PostConstruct
+	public void init() {
+		questionList = dataBean.getQuestionList();
+	}
 }
